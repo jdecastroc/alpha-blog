c646a41a3deec7997f1e8173e65eddbaf5ea862d
jdecastroc <jdecastrocabello@gmail.com>
2017-05-13 15:41:59 +0000

    Extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     
     def index
         @articles = Article.all #grab all the articles from the db
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
     end
     
     def update #When editing and updating object
-        @article = Article.find(params[:id]) #can be shown in the rake routes command
+        set_article #can be shown in the rake routes command
         if @article.update(article_params)  #Check the validations and update whit the params 
             flash[:notice] = "Article was successfully edited" 
             redirect_to article_path(@article)
@@ -30,21 +31,25 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id]) #get ID from the params of the URL
+        set_article
     end
     
     def destroy
-        @article = Article.find(params[:id])
+        set_article
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
     end
     
     def edit #Fire update when submit
-        @article = Article.find(params[:id]) #get ID from the params of the URL
+        set_article
     end
     
     private
+        def set_article
+            @article = Article.find(params[:id]) #get ID from the params of the URL
+        end
+        
         def article_params
            params.require(:article).permit(:title, :description)
         end
